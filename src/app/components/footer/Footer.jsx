ab3cfc1ba3872199e31542f9a8bb8e76c91e29fc
cristalPyC <cristaltn03@gmail.com>
2024-04-03 11:34:43 -0400

    fixing up imgs

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -1,13 +1,15 @@
 "use client"
 import { useState } from "react";
-import { Zoom} from "react-awesome-reveal";
+import { Zoom } from "react-awesome-reveal";
 import "./footer.scss";
+import 'animate.css';
 import { Form, Formik, ErrorMessage, Field } from "formik";
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import { useRouter } from "next/navigation";
 
 export const Footer = () => {
     const [emailPlaceholder, setEmail] = useState("You email");
+    const [message, setMessage] = useState(false);
     const [errorClassname, setErrorclassname] = useState(true);
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     const router = useRouter();
@@ -26,10 +28,8 @@ export const Footer = () => {
             error.email = "Please enter a valid email adress";
             setErrorclassname(false);
 
-        } else{
-            error.email = "Check you email ⭐";
-            setErrorclassname(true);
         }
+        
         return error;
     };
 
@@ -47,8 +47,14 @@ export const Footer = () => {
             <Formik
                 initialValues = {{email: ''}}
                 validate={handleValidation}
-                onSubmit = {(values) => {
+                onSubmit = {(values, {resetForm}) => {
+                    setTimeout(() => {
+                        setMessage(false);
+                    }, 2000);
+                    setMessage(true);
                     console.log(values);
+                    resetForm();
+                    
                 }}
             >
                 <Form className="form">
@@ -62,6 +68,7 @@ export const Footer = () => {
                         <button type="submit">Subscribe</button>
                     </div>
                     <ErrorMessage className={errorClassname ? "check__message" : "error__message"} name="email" component="div"/>
+                    <div className={message ? "check__message animate__animated animate__fadeIn" : "invisible"}>{message ? "Check you email ⭐" : ""}</div>
                 </Form>
             </Formik>
         </div>
@@ -77,7 +84,7 @@ export const Footer = () => {
                     <svg onClick={() => router.push("/login")} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 14.5V16.5M7 10.0288C7.47142 10 8.05259 10 8.8 10H15.2C15.9474 10 16.5286 10 17 10.0288M7 10.0288C6.41168 10.0647 5.99429 10.1455 5.63803 10.327C5.07354 10.6146 4.6146 11.0735 4.32698 11.638C4 12.2798 4 13.1198 4 14.8V16.2C4 17.8802 4 18.7202 4.32698 19.362C4.6146 19.9265 5.07354 20.3854 5.63803 20.673C6.27976 21 7.11984 21 8.8 21H15.2C16.8802 21 17.7202 21 18.362 20.673C18.9265 20.3854 19.3854 19.9265 19.673 19.362C20 18.7202 20 17.8802 20 16.2V14.8C20 13.1198 20 12.2798 19.673 11.638C19.3854 11.0735 18.9265 10.6146 18.362 10.327C18.0057 10.1455 17.5883 10.0647 17 10.0288M7 10.0288V8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8V10.0288" stroke="#77767c" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
                 </div>
                 <p>
-                    Copyright ©2024 | Design by Cristal Tavárez Novas
+                    Copyright ©2024 | By Cristal Tavárez Novas
                 </p>
             </div>
         </div>
